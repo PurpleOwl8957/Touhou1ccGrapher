0fcb64812d5358dd3594bb6d5428524f3434dda3
PurpleOwl8957 <murasakifukurou13@gmail.com>
2025-11-09 04:26:41 +0800

    Added p5.js library and draw a circle on the graphing area

--- main/main.js
+++ main/main.js
@@ -25,3 +25,24 @@ document.getElementById("ui-sidebar").style.width = ui_sidebar_width + "%";
 
 document.getElementById("ui-sidebar-content").style.height = ui_sidebar_height + "%";
 document.getElementById("ui-sidebar-content").style.width = (ui_sidebar_width - 2) + "%";
+
+// All the drawing and interactive stuff
+// Setup runs once at the start of the website load
+function setup() {
+	let canvas_width = nav_bar_width * windowWidth * 0.01;
+	let canvas_height = main_space_height * windowHeight * 0.01;
+	
+	console.log(canvas_width, canvas_height);
+	
+	let canvas = createCanvas(canvas_width, canvas_height);
+	canvas.position(0, 0);
+	canvas.parent("main-space");
+}
+
+// Draw runs every frame
+function draw() {
+	background('blue');
+	strokeWeight(5);
+	fill(color('red'))
+	circle(200, 200, 100);
+}
