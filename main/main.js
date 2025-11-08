d0fc1bc1e6af68624ec90622ba50c2b1d454d437
PurpleOwl8957 <murasakifukurou13@gmail.com>
2025-11-08 14:45:34 +0800

    Updated CSS and JS

--- main/main.js
+++ main/main.js
@@ -7,29 +7,21 @@ let nav_bar_width = 80;
 let main_space_width = nav_bar_width;
 let ui_sidebar_width = 100 - nav_bar_width;
 
-let padding = 1;
-
 // Assigning all CSS properties
 document.getElementById("top-nav-bar").style.height = nav_bar_height + "%";
 document.getElementById("top-nav-bar").style.width = nav_bar_width + "%";
 
 document.getElementById("top-nav-bar-content").style.height = nav_bar_height + "%";
 document.getElementById("top-nav-bar-content").style.width = nav_bar_width + "%";
-document.getElementById("top-nav-bar-content").style.padding = padding + "%";
 
 document.getElementById("main-space").style.height = main_space_height + "%";
 document.getElementById("main-space").style.width = main_space_width + "%";
 
 document.getElementById("main-content").style.height = (main_space_height - 5) + "%";
 document.getElementById("main-content").style.width = (main_space_width - 2) + "%";
-document.getElementById("main-content").style.paddingLeft = padding + "%";
-document.getElementById("main-content").style.paddingRight = padding + "%";
-document.getElementById("main-content").style.paddingBottom = padding + "%";
 
 document.getElementById("ui-sidebar").style.height = ui_sidebar_height + "%";
 document.getElementById("ui-sidebar").style.width = ui_sidebar_width + "%";
 
 document.getElementById("ui-sidebar-content").style.height = ui_sidebar_height + "%";
 document.getElementById("ui-sidebar-content").style.width = (ui_sidebar_width - 2) + "%";
-document.getElementById("ui-sidebar-content").style.paddingTop = padding + "%";
-document.getElementById("ui-sidebar-content").style.paddingRight = padding + "%";
